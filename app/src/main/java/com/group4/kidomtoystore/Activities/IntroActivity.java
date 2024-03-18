b76145722b42b56d630e323bb1047879e23b7db5
Uyen <nnphuyen563@gmail.com>
2024-03-18 16:58:23 +0700

    arrange folder

--- app/src/main/java/com/group4/kidomtoystore/Activities/IntroActivity.java
+++ app/src/main/java/com/group4/kidomtoystore/Activities/IntroActivity.java
@@ -1,14 +1,16 @@
-package com.group4.kidomtoystore;
+package com.group4.kidomtoystore.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.group4.kidomtoystore.R;
+
+public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.intro_main);
     }
 }
