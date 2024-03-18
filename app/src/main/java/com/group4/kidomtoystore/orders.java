e1cb0f2beabac54fd641e1e149291ce978b56712
tammchannn <anhtamtat2611@gmail.com>
2024-03-18 17:35:06 +0700

    orders, item order list

---
+++ app/src/main/java/com/group4/kidomtoystore/orders.java
@@ -0,0 +1,29 @@
+package com.group4.kidomtoystore;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.group4.kidomtoystore.Adapters.ViewPagerAdapter;
+
+public class orders extends AppCompatActivity {
+    private TabLayout tablayout;
+    private ViewPager viewpager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orders);
+
+        tablayout = findViewById(R.id.tlOrder);
+        viewpager = findViewById(R.id.vpOrder);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewpager.setAdapter(viewPagerAdapter);
+
+        tablayout.setupWithViewPager(viewpager);
+    }
+}
