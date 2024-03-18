b30b6bbaf620d2e89f679c5c632d2009f9285ae7
phamanhthu411 <thupa21411@st.uel.edu.vn>
2024-03-19 02:40:47 +0700

    thêm màn onboard 1

---
+++ app/src/main/java/com/group4/kidomtoystore/On_Boarding.java
@@ -0,0 +1,19 @@
+package com.group4.kidomtoystore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.group4.kidomtoystore.databinding.ActivityOnBoardingBinding;
+
+public class On_Boarding extends AppCompatActivity {
+
+    ActivityOnBoardingBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+    }
+}
