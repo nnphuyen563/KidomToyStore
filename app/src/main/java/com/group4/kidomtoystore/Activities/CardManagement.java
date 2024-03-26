e17a7343672548b3e34e67fbf8c8c5fe80f7e015
Ngoc Anh <anhnh21411@st.uel.edu.vn>
2024-03-26 18:29:48 +0700

    Adress, card

---
+++ app/src/main/java/com/group4/kidomtoystore/Activities/CardManagement.java
@@ -0,0 +1,16 @@
+package com.group4.kidomtoystore.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.group4.kidomtoystore.R;
+
+public class CardManagement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_management);
+    }
+}
